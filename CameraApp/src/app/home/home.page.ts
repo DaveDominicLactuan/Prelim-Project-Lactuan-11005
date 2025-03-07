742f433c7965ae0ce3f847817dc60fe2fdc3d55a
DaveDominicLactuan <davedominic.lactuan.22@usjr.edu.ph>
2025-03-07 10:26:47 +0800

    camera

---
+++ CameraApp/src/app/home/home.page.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
+})
+export class HomePage {
+    imageurl: string | undefined;
+
+    async takePicture() {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Camera,
+      });
+      this.imageurl = image.webPath;
+    }
+
+    async selectFromGallery(){
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Photos,
+      })
+      this.imageurl = image.webPath;
+    }
+
+    async takeAndEditPicture(){
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: true,
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Camera,
+      });
+      this.imageurl = image.webPath;
+    }
+    
+
+  constructor() {}
+
+}
