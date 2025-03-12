f4f2be97c01b6bdb97e96cd1aa310e1b56ac0baa
DaveDominicLactuan <davedominic.lactuan.22@usjr.edu.ph>
2025-03-12 21:26:46 +0800

    AppPreference

--- appPreferences/src/app/app.component.ts
+++ appPreferences/src/app/app.component.ts
@@ -1,48 +1,42 @@
-import { Component } from '@angular/core';
-import { IonCheckbox } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
 import { Preferences } from '@capacitor/preferences';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
-
+  standalone: false,
 })
-
-export class AppComponent {
-  toggle1: boolean = false;
-  toggle2: boolean = false;
-  toggle3: boolean = false;
-  rangeValue: number = 0;
-  checkbox1: boolean = false;
-  checkbox2: boolean = false;
-  checkbox3: boolean = false;
-  radioOption1: boolean = false;
-  radioOption2: boolean = false;
+export class AppComponent implements OnInit {
+  toggleOptions: boolean[] = [false, false, false];
+  rangeValue: number = 50;
+  checkboxes: boolean[] = [false, false, false, false, false];
 
   async ngOnInit() {
-    this.toggle1 = (await Preferences.get({ key: 'toggle1' })).value === 'true';
-    this.toggle2 = (await Preferences.get({ key: 'toggle2' })).value === 'true';
-    this.toggle3 = (await Preferences.get({ key: 'toggle3' })).value === 'true';
-    this.rangeValue = parseInt((await Preferences.get({ key: 'rangeValue' })).value || '0');
-    this.checkbox1 = (await Preferences.get({ key: 'checkbox1' })).value === 'true';
-    this.checkbox2 = (await Preferences.get({ key: 'checkbox2' })).value === 'true';
-    this.checkbox3 = (await Preferences.get({ key: 'checkbox3' })).value === 'true';
-    this.radioOption1 = (await Preferences.get({ key: 'radioOption1' })).value === 'true';
-    this.radioOption2 = (await Preferences.get({ key: 'radioOption2' })).value === 'true';
+    await this.loadPreferences();
   }
 
-  async saveToggle(key: string, event: any) {
-    await Preferences.set({ key, value: event.detail.checked.toString() });
+  async loadPreferences() {
+    const toggleData = await Preferences.get({ key: 'toggleOptions' });
+    if (toggleData.value) this.toggleOptions = JSON.parse(toggleData.value);
+
+    const rangeData = await Preferences.get({ key: 'rangeValue' });
+    if (rangeData.value) this.rangeValue = JSON.parse(rangeData.value);
+
+    const checkboxData = await Preferences.get({ key: 'checkboxes' });
+    if (checkboxData.value) this.checkboxes = JSON.parse(checkboxData.value);
   }
 
-  async saveRange(key: string, event: any) {
-    await Preferences.set({ key, value: event.detail.value.toString() });
+  async savePreferences() {
+    await Preferences.set({ key: 'toggleOptions', value: JSON.stringify(this.toggleOptions) });
+    await Preferences.set({ key: 'rangeValue', value: JSON.stringify(this.rangeValue) });
+    await Preferences.set({ key: 'checkboxes', value: JSON.stringify(this.checkboxes) });
   }
 
-  async saveCheckbox(key: string, event: any) {
-    await Preferences.set({ key, value: event.detail.checked.toString() });
-  
-}
-
+  async resetPreferences() {
+    this.toggleOptions = [false, false, false];
+    this.rangeValue = 50;
+    this.checkboxes = [false, false, false, false, false];
+    await this.savePreferences();
+  }
 }
--- appPreferences/src/app/app.module.ts
+++ appPreferences/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
